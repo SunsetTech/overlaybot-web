eb620dea6b739e172beeafdf0c549b2bdaf6282e
Sunset <777919+SunsetTech@users.noreply.github.com>
2026-08-01 15:28:17 -0400

    Fixes disappearing login page bug

--- packages/client/src/Login.tsx
+++ packages/client/src/Login.tsx
@@ -1,22 +1,23 @@
 import { useCookies } from "react-cookie"
 import { Button } from "./components/ui/button"
+
 function Login() {
-	const [_, SetCookie] = useCookies(['OAuthState'])
-	const StateToken = crypto.randomUUID()
-	SetCookie(
-		"OAuthState", StateToken, {
+	const [_, setCookie] = useCookies(['OAuthState'])
+	const ClientID = import.meta.env.VITE_TWITCH_CLIENT_ID
+	const RedirectURI = import.meta.env.VITE_TWITCH_REDIRECT_URI
+
+	const handleClick = () => {
+		const stateToken = crypto.randomUUID()
+		setCookie("OAuthState", stateToken, {
 			path: "/",
 			sameSite: "lax",
 			maxAge: 300,
 			secure: true
-		}
-	)
-	const ClientID = import.meta.env.VITE_TWITCH_CLIENT_ID
-	const RedirectURI = import.meta.env.VITE_TWITCH_REDIRECT_URI
-	const URL = `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${ClientID}&redirect_uri=${RedirectURI}&scope=user%3Aread%3Aemail&state=${StateToken}`
-	return <Button asChild>
-		<a href={URL}>Connect with Twitch</a>
-	</Button>
-}
+		})
+		const url = `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${ClientID}&redirect_uri=${RedirectURI}&scope=user%3Aread%3Aemail&state=${stateToken}`
+		window.location.href = url
+	}
 
+	return <Button onClick={handleClick}>Connect with Twitch</Button>
+}
 export default Login
